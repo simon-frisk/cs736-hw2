56ab2fc26df6f579a1c08f1a4011abe49fda04ad
Simon Frisk <simon@simonfrisk.com>
2024-10-11 17:33:03 -0500

    f

---
+++ main.py
@@ -0,0 +1,34 @@
+import itertools
+import subprocess
+
+def main():
+    jobFile = "jobFileCreate.fio"
+    createJobFile(jobFile)
+    runJobs(jobFile)
+
+def createJobFile(fileName):
+    with open(fileName, 'w') as file:
+        file.write("[global]\nioengine=psync\nbs=4k\nsize=1G\nruntime=30s\ntime_based\ndirect=1\n")
+
+        drivers=['/dev/nullb0','/dev/rnullb0']
+        xDim = ["write", "read"]
+        blockSizes = ['4','8']
+        numJobs = ['1','2']
+        
+        for (mode, blockSize, jobs, driver) in itertools.product(xDim, blockSizes, numJobs, drivers):
+            file.write(f"[{driver}-{mode}-{blockSize}-{jobs}]\n")
+            file.write(f"filename={driver}\n")
+            file.write(f"readwrite={mode}\n")
+            file.write(f"numjobs={jobs}\n")
+            file.write(f"blockSize={blockSize}\n")
+            file.write(f"\n")
+
+        
+
+
+def runJobs(jobFile):
+    subprocess.run(['sudo', 'fio', jobFile], capture_output=True, text=True)
+
+
+if __name__ == '__main__':
+    main()  
